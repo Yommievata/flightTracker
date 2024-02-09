9ecfc93e2d88cc8adcf3d2754bba0f8f67cb892c
OmotolaniGbodo <olusanyaabayomi@yahoo.com>
2024-02-09 09:57:44 +0100

    Added doc

--- src/main/java/com/flightapi/flightTracker/service/FlightService.java
+++ src/main/java/com/flightapi/flightTracker/service/FlightService.java
@@ -19,6 +19,21 @@ public class FlightService {
         this.restTemplate = restTemplate;
     }
 
+    /**
+     * Sends a request for a round trip flight using the specified parameters.
+     *
+     * @param  apiKey        the API key for authentication
+     * @param  origin        the origin airport code
+     * @param  destination   the destination airport code
+     * @param  departureDate the departure date in the format "YYYY-MM-DD"
+     * @param  returnDate    the return date in the format "YYYY-MM-DD"
+     * @param  adults        the number of adult passengers
+     * @param  children      the number of child passengers
+     * @param  infants       the number of infant passengers
+     * @param  cabinClass    the cabin class for the flight
+     * @param  currency      the currency for the pricing
+     * @return               the response from the API as a String
+     */
     public String roundTrip(String apiKey, String origin, String destination, String departureDate, String returnDate,
                             int adults, int children, int infants, String cabinClass, String currency) {
         String roundTripUrl = apiUrl + "roundtrip/" + apiKey + "/" + origin + "/" + destination + "/"
@@ -28,6 +43,21 @@ public class FlightService {
         return restTemplate.getForObject(roundTripUrl, String.class);
     }
 
+    /**
+     * Sends a request for a one-way trip using the provided API key, origin, destination, departure date,
+     * number of adults, children, infants, cabin class, and currency.
+     *
+     * @param  apiKey         the API key for authentication
+     * @param  origin         the origin airport code
+     * @param  destination    the destination airport code
+     * @param  departureDate  the date of departure in the format "YYYY-MM-DD"
+     * @param  adults         the number of adult passengers
+     * @param  children       the number of child passengers
+     * @param  infants        the number of infant passengers
+     * @param  cabinClass     the cabin class for the trip
+     * @param  currency       the currency for the trip
+     * @return                the response from the API as a String
+     */
     public String oneWayTrip(String apiKey, String origin, String destination, String departureDate,
                              int adults, int children, int infants, String cabinClass, String currency) {
         String oneWayTripUrl = apiUrl + "onewaytrip/" + apiKey + "/" + origin + "/" + destination + "/"
